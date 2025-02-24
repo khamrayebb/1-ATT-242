9614a18fe387eecb7a44e54718ce8b0ba8d1d09f
khamrayevbb <khamrayevbb@gmail.com>
2025-02-24 09:33:54 +0500

    asdhdgsah

---
+++ Новая папка/main.py
@@ -0,0 +1,75 @@
+# a = list(range(15))
+# print(a)
+
+
+
+# for item in range(6):
+    # print("hello")
+    # print(item)
+
+
+
+# numbers = [255,654,87878]
+
+# for number in numbers:
+#     print(number)
+
+
+
+# buyurtma = []
+
+# for i in range(3):
+#     a = input(f"{i+1} buyurtma: ")
+#     buyurtma.append(a)
+# print(buyurtma)
+
+
+# a = 0
+
+# if a>0:
+#     print("musbat")
+# elif a<0:
+#     print("manfiy")
+# else:
+#     print("nolga teng")
+
+
+# numbers = [255,654,87878,84845,88]
+
+# for number in range(3):
+#     a = int(input('son kirit: '))
+#     if a in numbers:
+#         numbers.remove(a)
+#     else:
+#         numbers.append(a)
+# print(numbers)
+
+
+# a = {
+#     'name':"Ali",
+#     'age':20,
+#     'status':True
+# }
+# a['ball']=200
+# print(a)
+# print(a['agee'])
+# print(a.get('statuss'))
+
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+
+# products = {}
+
+# for i in range(1,4):
+#     a = input(f'{i}-product: ')
+#     b= input(f'{a} price: ')
+#     products[a]=b
+# print(products)
+
+
+
+
+
+
+
